b42e915d0c03b283631d406f989d6aa4d87cfb40
hai@leftcoastlogic.com <hai@leftcoastlogic.com>
2017-06-21 13:51:57 +0700

    - Support Carthage

---
+++ BLKBar/BLKBar.h
@@ -0,0 +1,19 @@
+//
+//  BLKBar.h
+//  BLKBar
+//
+//  Created by Nguyen Thanh Hai on 6/21/17.
+//  Copyright © 2017 Bryan Keller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BLKBar.
+FOUNDATION_EXPORT double BLKBarVersionNumber;
+
+//! Project version string for BLKBar.
+FOUNDATION_EXPORT const unsigned char BLKBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLKBar/PublicHeader.h>
+
+
